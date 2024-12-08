4e442c1119cc437d830ead1d8d883b4bb50596fc
Sahed2006 <130493694+Sahed2006@users.noreply.github.com>
2024-12-08 22:40:12 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -29,18 +29,18 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
 𝐍𝐚𝐦𝐞       : 𝐒𝐀𝐇𝐄𝐃 𝐇𝐎𝐒𝐒𝐄𝐍
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : 𝐒𝐀𝐇𝐄𝐃 𝐇𝐎𝐒𝐒𝐄𝐍
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : 𝐀𝐧𝐝𝐫𝐞𝐰 𝐋𝐲𝐫𝐢𝐜𝐬
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
-𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐅𝐚𝐫𝐢𝐝𝐩𝐮𝐫,  𝐃𝐡𝐚𝐤𝐚
-𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐅𝐚𝐫𝐢𝐝𝐩𝐮𝐫,  𝐃𝐡𝐚𝐤𝐚
+𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐑𝐚𝐣𝐛𝐚𝐫𝐢,𝐃𝐡𝐚𝐤𝐚
+𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐑𝐚𝐣𝐛𝐚𝐫𝐢,𝐃𝐡𝐚𝐤𝐚
 𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
 𝐀𝐠𝐞           : 𝟏𝟖+
-𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
+𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐮𝐦𝐚𝐢𝐲𝐚'𝐬 𝐇𝐮𝐬𝐛𝐚𝐧𝐝 😌❤️‍🩹
 𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
 𝐆𝐦𝐚𝐢𝐥       : mentalsahed@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/01919568467
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/01739597712
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : https://t.me/sahed2006
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/sahed.hossen.829`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/Sumaiya.ily.bbe`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(encodeURI(`https://graph.facebook.com/100000959749712/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
